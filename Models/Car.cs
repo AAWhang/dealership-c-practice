852da6a4dacb84181375a4c1c05bd3929d30f791
Aaron Whang and Holden Clark <holden@>
2019-07-17 15:24:27 -0700

    it will add the old version in comments for comparison

--- Models/Car.cs
+++ Models/Car.cs
@@ -28,3 +28,51 @@ namespace Dealership.Models
 
   }
 }
+
+// namespace Dealership.Models
+// {
+//   class Car
+//   {
+//     private string _makeModel;
+//     private int _price;
+//     private int _miles;
+
+//     public Car(string makeModel, int price, int miles)
+//     {
+//       _makeModel = makeModel;
+//       _price = price;
+//       _miles = miles;
+//     }
+
+//     public string GetMakeModel()
+//     {
+//       return _makeModel;
+//     }
+
+//     public int GetPrice()
+//     {
+//       return _price;
+//     }
+
+//     public int GetMiles()
+//     {
+//       return _miles;
+//     }
+
+//     public void SetPrice(int newPrice)
+//     {
+//       _price = newPrice;
+//     }
+
+//     public bool WorthBuying(int maxPrice)
+//     {
+//       return (_price < maxPrice);
+//     }
+
+//     public static string MakeSound(string sound)
+//     {
+//       return "Our cars sound like " + sound;
+//     }
+
+//   }
+// }
